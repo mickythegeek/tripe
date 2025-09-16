c7a08294c4447356063649b54ee3234cc66ed0c5
Michael David <lordkay07@gmail.com>
2025-09-17 00:20:56 +0100

    feat: Add admin logout functionality and improve dashboard

--- tripe-backend/resources/views/admin/dashboard.blade.php
+++ tripe-backend/resources/views/admin/dashboard.blade.php
@@ -161,7 +161,7 @@
 											
 											<li id="menu-item-29-2VewETdxAb" class="menu-item-divider"></li>
 											<li class="menu-item menu-item-hoverable gap-2 h-[35px]">
-                                                <a class="flex gap-2 items-center" href="admin-login.html">
+                                                <a class="flex gap-2 items-center" href="{{ route('admin_logout') }}">
                                                     <span class="text-xl opacity-50">
                                                         <svg stroke="currentColor" fill="none" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path>
