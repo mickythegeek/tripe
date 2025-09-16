fa05c5cc7c7655261d1762e7bddc75f893cad394
simeon <simeonolawale69@gmail.com>
2025-09-16 18:48:12 +0100

    Remove submodule and add my-react as a normal folder

--- my-react/src/Components/SignIN/SignIN.js
+++ my-react/src/Components/SignIN/SignIN.js
@@ -5,67 +5,88 @@ import PasswwordIcon from "../Assets/Icon.png";
 import EmailIcon from "../Assets/Icon (1).png";
 import Logomark from "../Assets/Logomark.png";
 import SignOut from "../Assets/SignOut.png";
+import { MdCopyright } from "react-icons/md";
 import "./SignIN.css";
 
 function SignIN() {
   return (
-    <div className="SignIN">
-      <div className="logo">
-        <img src={Logomark} alt="logo" />
-        <h1>Tripe</h1>
-      </div>
-      <h3>Sign In To Your Account</h3>
-      <p>Unleash your inner sloth 4.0 right now.</p>
-      <form>
-        <label>Email Address</label>
-        <div className="inputwrapper">
-          <img src={EmailIcon} />
-          <input type="email" placeholder="Enter your email" /> <br />
+    <div>
+      <div className="SignIN">
+        <div className="logo">
+          <img src={Logomark} alt="logo" />
+          <h1>Tripe</h1>
         </div>
-        <label>Password</label>
-        <div className="inputwrapper">
-          <img src={PasswwordIcon} />
-          <input type="password" placeholder="Enter your password" />
-        </div>
+        <h3>Sign In To Your Account</h3>
+        <p>Unleash your inner sloth 4.0 right now.</p>
+        <form>
+          <label>Email Address</label>
+          <div className="inputwrapper">
+            <img src={EmailIcon} />
+            <input type="email" placeholder="Enter your email" /> <br />
+          </div>
+          <label>Password</label>
+          <div className="inputwrapper">
+            <img src={PasswwordIcon} />
+            <input type="password" placeholder="Enter your password" />
+          </div>
 
-        <div className="options">
-          <span>
-            <input type="checkbox" /> <label>Remember For 30Days</label>
-          </span>
-          <span>
-            <a href="#"> Forget Password </a>
-          </span>
-        </div>
-        <div>
-          <a href="#" className="SignInBTN">
-            <button className="BTN1">
-              Sign In
-              <img src={SignOut} alt="logo" className="SignOut" />
+          <div className="options">
+            <span>
+              <input type="checkbox" /> <label>Remember For 30Days</label>
+            </span>
+            <span>
+              <a href="#"> Forget Password </a>
+            </span>
+          </div>
+          <div>
+            <a href="#" className="SignInBTN">
+              <button className="BTN1">
+                Sign In
+                <img src={SignOut} alt="logo" className="SignOut" />
+              </button>
+            </a>
+          </div>
+
+          <p className="signuptext">
+            Don't have an account? <span>Sign Up</span>
+          </p>
+
+          <div className="hrwithtext">
+            <hr />
+            <span>OR</span>
+            <hr />
+          </div>
+
+          <div className="socialmedia">
+            <button>
+              <img src={Facebookicon} alt="icon" /> Sign In With Google
             </button>
-          </a>
-        </div>
+            <button>
+              <img src={Twittericon} alt="icon" /> Sign In With Twitter
+            </button>
+          </div>
+        </form>
+      </div>
+      <footer className="footer">
+        <hr></hr>
+        <div className="footercontent">
+          <div className="copyright">
+            <p>
+              Copyright 2025 Tripe
+            </p>
+               <p> <MdCopyright /></p>
+          </div>
 
-        <p className="signuptext">
-          Don't have an account? <span>Sign Up</span>
-        </p>
-
-        <div className="hrwithtext">
-          <hr />
-          <span>OR</span>
-          <hr />
+          <div className="footerlinks">
+            <div>
+              <p>Privacy Policy</p>
+            </div>
+            <div>
+              <p>Terms & Conditions </p>
+            </div>
+          </div>
         </div>
-
-        <div className="socialmedia">
-          <button>
-          
-            <img src={Facebookicon} alt="icon" /> Sign In With Google
-          </button>
-          <button>
-          
-            <img src={Twittericon} alt="icon" /> Sign In With Twitter
-          </button>
-        </div>
-      </form>
+      </footer>
     </div>
   );
 }
