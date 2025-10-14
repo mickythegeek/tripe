a01384393a9fb9d5c3d770f4fe326469515a6446
Michael David <lordkay07@gmail.com>
2025-10-14 09:57:44 +0100

    feat: Add Swagger UI for API documentation

--- tripe-backend/app/Http/Controllers/Controller.php
+++ tripe-backend/app/Http/Controllers/Controller.php
@@ -2,6 +2,35 @@
 
 namespace App\Http\Controllers;
 
+
+/** 
+ *@OA\Info(
+ *     version="1.0.0",
+ *     title="Tripe Auth API Docs",
+ *     description="This is the official API documentation for the Tripe Authentication System. It provides detailed information about tall the available endpoints.",
+ *     @OA\Contact(
+ *         email="dev@tripe.com"
+ *     ),
+ *     @OA\License(
+ *         name="MIT License",
+ *     url="https://opensource.org/Licenseses/MIT"
+ *     )
+ * )
+ * 
+ * @OA\Server(
+ *     url="http://localhost:8000",
+ *     description="Tripe API Server"
+ * )
+ * 
+ * @OA\SecurityScheme(
+ *     securityScheme="bearerAuth",
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="JWT",
+ *     description="Enter token in format: Bearer {token}"
+ * )
+ * 
+*/
 abstract class Controller
 {
     //
